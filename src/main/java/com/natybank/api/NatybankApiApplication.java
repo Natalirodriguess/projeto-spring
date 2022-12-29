d21ce51757dbcb5115b77c0e733478fc53bc14a3
thomas.chaves <thomas.chaves@rpe.tech>
2022-12-28 22:19:17 -0300

    Setup do ambiente

---
+++ src/main/java/com/natybank/api/NatybankApiApplication.java
@@ -0,0 +1,13 @@
+package com.natybank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatybankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatybankApiApplication.class, args);
+	}
+
+}
